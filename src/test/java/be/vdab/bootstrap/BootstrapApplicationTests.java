995d3c870c439d12b87e29e11d0393a9a6208bac
yuliyaBir <zhivo4ka@gmail.com>
2024-01-16 10:05:09 +0100

    Initial commit

---
+++ src/test/java/be/vdab/bootstrap/BootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
